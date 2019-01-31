109165688abdc537f43e9bfa9932c67c756d38a0
ishrat singh <ishratsingh12@gmail.com>
2019-01-31 14:15:07 -0500

    Update server.js for openshift

--- server.js
+++ server.js
@@ -29,8 +29,12 @@ io.on("connection", client => {
     });
 });
 
-const port = 8000;
-server.listen(port, function(err) {
-    if (err) throw err;
-    console.log("listenting on port :" + port);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+app.listen(server_port, server_ip_address, function () {
+
+    console.log( "Listening on " + server_ip_address + ", server_port " + server_port  );
+
 });
